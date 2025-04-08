bd206aa000613f2c79f786744a559bdd3e0c10ad
Jason Chen <jasonschen3@gmail.com.>
2025-04-08 23:20:18 +0200

    fix: shift firebase config into env

--- frontend/src/firebase/firebaseConfig.ts
+++ frontend/src/firebase/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCC-Kews6zKmGbqEGqqFAio6nzLoHhW1yM",
-  authDomain: "blackjack-f78d6.firebaseapp.com",
-  projectId: "blackjack-f78d6",
-  storageBucket: "blackjack-f78d6.firebasestorage.app",
-  messagingSenderId: "120417293528",
-  appId: "1:120417293528:web:23840aca18c76f9a7873ad",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase (no need to export since I'm not using)
